f15873ee5da1acf63ed76505ebc7c35007795269
karan <karanverma201411@gmail.com>
2024-12-20 21:13:26 +0530

    leetcode : price with discount solution

--- easy/priceWithDiscount.js
+++ easy/priceWithDiscount.js
@@ -1,33 +1,23 @@
 const finalPrices = function (prices) {
   const discountArr = [];
-  //   let isPushed = false;
-  //   for (let i = 0; i <= prices.length - 1; i++) {
-  //     for (j = i + 1; j <= prices.length - 2; j++) {
-  //       if (j > i && prices[j] <= prices[i]) {
-  //         discountArr.push(prices[i] - prices[j]);
-  //         isPushed = true;
-  //       }
-  //     }
-  //     discountArr.push(prices[i]);
-  //   }
 
   for (let i = 0; i <= prices.length - 1; i++) {
-    for (let j = i + 1; j <= prices.length - 2; j++) {
+    let isPushed = false;
+    for (let j = i + 1; j <= prices.length - 1; j++) {
       if (j > i) {
         if (prices[j] <= prices[i]) {
           discountArr.push(prices[i] - prices[j]);
-          j = prices.length;
+          isPushed = true;
+          break;
         }
       }
     }
-    // discountArr.push(prices[i]);
+    if (isPushed === false) {
+      discountArr.push(prices[i]);
+    }
   }
   return discountArr;
 };
 
 const result = finalPrices([9, 3, 9, 9, 4, 7, 1, 8, 6]);
-console.log('result', result); //expected : [6,2,0,5,3,6,1,2,6]
-
-/* 8-4 = 4
-
-*/
+console.log('result', result);
