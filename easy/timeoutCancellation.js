65c7536658c6c7edce90bbcebe8cd31321e82858
karan <karanverma201411@gmail.com>
2024-12-30 07:27:55 +0530

    leetcode : timeout cancellation

--- easy/timeoutCancellation.js
+++ easy/timeoutCancellation.js
@@ -1,15 +1,26 @@
 var cancellable = function (fn, args, t) {
-  const cancelMs = 3000;
-  const cancelFn = () => {
-    console.log('cancel fn insdie');
-  };
+  const timer = setTimeout(() => {
+    fn(...args);
+  }, t);
 
-  return cancelMs > t
-    ? setTimeout(() => {
-        console.log(fn(args));
-      }, t)
-    : setTimeout(cancelFn, cancelMs);
+  return function cancelFn() {
+    clearTimeout(timer);
+  };
 };
 
-const addNums = (x) => x + 4;
-cancellable(addNums, 3, 5000);
+const result = [];
+const cancelTimeMs = 50;
+const fn = (x) => x * 5;
+const t = 20;
+const args = [2];
+const startTime = Math.floor(performance.now());
+
+if (t < cancelTimeMs) {
+  result.push({ time: startTime - cancelTimeMs, returned: fn(...args) });
+}
+
+const returnedCancelFn = cancellable(fn, args, t);
+setTimeout(() => {
+  returnedCancelFn();
+  console.log(result);
+}, cancelTimeMs);
