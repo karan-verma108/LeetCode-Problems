4efd1b31a4cdd97016f443c2d6c57d22c8281e58
karan <karanverma201411@gmail.com>
2025-01-08 06:23:23 +0530

    leetcode : started with generate fibonacci

---
+++ easy/generateFibonacci.js
@@ -0,0 +1,2 @@
+var fibGenerator = function* () {}; 
+//the star symbol represents that it's a generator function, a generator function is a special function that can be paused and resumed during its execution,
