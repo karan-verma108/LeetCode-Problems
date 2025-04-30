7be83ac8b651058b0f4c1641e25b4f8a7b20bbff
karan <karanverma201411@gmail.com>
2025-04-30 23:49:15 +0530

    remove element question solved

--- easy/removeElement.js
+++ easy/removeElement.js
@@ -1,1 +1,29 @@
 // 27. Remove Element
+
+const removeElement = function (nums, val) {
+  let k = 0;
+  if (nums.length > 0) {
+    console.log('nums earlier', nums);
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === val) {
+        nums.splice(i, 1);
+        i--;
+      }
+    }
+
+    k = nums.length;
+    // console.log('k value', k);
+
+    for (let i = 0; i < k; i++) {
+      nums.push('_');
+    }
+  } else {
+    console.log('no data in array');
+  }
+  return k;
+};
+
+const result = removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
+
+console.log('result', result);
