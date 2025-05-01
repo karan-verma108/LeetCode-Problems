6a12c2b2bc466061b985083f2bba7126e493533b
karan <karanverma201411@gmail.com>
2025-05-01 21:14:29 +0530

    another question solved

--- easy/toBeOrNot.js
+++ easy/toBeOrNot.js
@@ -1,1 +1,23 @@
 // 2704. To Be Or Not To Be
+
+const expect = function (val) {
+  return {
+    toBe: (value) => {
+      if (val === value) {
+        return true;
+      } else {
+        return 'Not Equal';
+      }
+    },
+    notToBe: (value) => {
+      if (val !== value) {
+        return true;
+      } else {
+        return 'Equal';
+      }
+    },
+  };
+};
+
+const result = expect(5).toBe(null);
+console.log(result);
